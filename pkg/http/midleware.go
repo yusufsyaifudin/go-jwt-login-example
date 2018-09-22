5399305aa68d6256353d96af75a272c43f44c0e9
Yusuf Syaifudin <yusuf.syaifudin@gmail.com>
2018-09-22 15:07:20 +0700

    Add comment in pkg/http/middleware.go

--- pkg/http/midleware.go
+++ pkg/http/midleware.go
@@ -2,7 +2,8 @@ package http
 
 import "context"
 
-// Implementing this idea https://hackernoon.com/simple-http-middleware-with-go-79a4ad62889b
+// Implementing this idea https://hackernoon.com/simple-http-middleware-with-go-79a4ad62889b.
+// The mw is an array consist of Middleware type.
 func ChainMiddleware(mw ...Middleware) Middleware {
 	return func(final Handler) Handler {
 		return func(ctx context.Context, req Request) Response {
